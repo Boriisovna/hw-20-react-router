666dd67e0e8534f960e5a3021b24bc21c3da62b0
Viktoriia Babiuk <cloud14777@gmail.com>
2021-08-15 14:03:06 +0300

    added second post

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,24 +1,40 @@
 import PostComponent from '../Post/PostComp';
 
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+const postPhoto = "https://pbs.twimg.com/media/E2ym_7VWYAY_RFU?format=jpg&name=large"
+
+
+const avatarPhoto = "https://pbs.twimg.com/profile_images/1348509746106671105/STdnNKB6_400x400.jpg"
+
+
+const posts=[
+{
+    name: "Anakin skywalker",
+    photo: ANAKIN_IMAGE,
+    nickname: "@dart_vader",
+    content:"WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image:RAY_IMAGE,
+    date:"26 февр."
+},
+ {
+  name: "Typical programmer",
+  photo: avatarPhoto,
+  nickname: "@tproger",
+  content:"When you try to choose a meaningful variable name:",
+  image:postPhoto,
+  date:" 15 сент."
+}
+]
+
 
 function Post() {
-  const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
-  const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
-  
+
   return ( 
     <div>
-        <PostComponent author={{
-              name: "Anakin skywalker",
-              photo: ANAKIN_IMAGE,
-              nickname: "@dart_vader"
-           }}
-           content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
-           image={RAY_IMAGE}
-           date={"26 февр."}
-        />
-
+      {posts.map((post)=><PostComponent name={post.name} photo={post.photo} nickname={post.nickname} content={post.content} image={post.image} date={post.date} key={post.name}/>)}
     </div>
   );
-}
+  }
 
 export default Post;
--- src/Post/PostComp.js
+++ src/Post/PostComp.js
@@ -6,7 +6,7 @@ import download from '../assets/download.svg'
 import bubble from '../assets/bub.svg'
 
 
-function PostComponent({author,content,image,date}) {
+function PostComponent(props) {
 
     
     return ( 
@@ -14,22 +14,22 @@ function PostComponent({author,content,image,date}) {
          <div className='post'>
              <header>
                  <div className='avatar'>
-                     <img src={author.photo} alt="" />
+                     <img src={props.photo} alt="" />
                  </div>
                  <div className='info'>
                      <div className="info_name">
-                         <span>{author.name}</span>
-                         <span className='nickname'>{author.nickname}</span>
-                         <span className='date'>{date}</span>
+                         <span>{props.name}</span>
+                         <span className='nickname'>{props.nickname}</span>
+                         <span className='date'>{props.date}</span>
                      </div>
                      <div className="info_text">
-                     <span>{content}</span>
+                     <span>{props.content}</span>
                      </div>
                  </div>
              </header>
              <main>
                  <div className="photo_post">
-                     <img src={image} alt="" />
+                     <img src={props.image} alt="" />
                  </div>
                  <div className="icons">
                  <span><img src={bubble} alt="" /> &nbsp; 82</span>
